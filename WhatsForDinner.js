2d2b92573f0e802d61d697e6cccc987ccf75936f
dawod0199 <39832780+dawod0199@users.noreply.github.com>
2018-11-15 11:42:03 -0500

    Create WhatsForDinner.js

---
+++ WhatsForDinner.js
@@ -0,0 +1,11 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+fill(255, 153, 0);
+ellipse(175,230,136,136);
+
+fill(102, 255, 51);
+ellipse(145,128,20,100);
+ellipse(175,121,20,100);
+ellipse(205,129,20,100);
