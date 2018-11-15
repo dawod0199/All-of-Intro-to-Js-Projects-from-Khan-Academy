81d10896a3cbb7fc5403728a2f57c8207859fd00
dawod0199 <39832780+dawod0199@users.noreply.github.com>
2018-11-15 11:47:21 -0500

    Create AdDesign.js

---
+++ AdDesign.js
@@ -0,0 +1,11 @@
+fill(255, 0, 0);
+textSize(30);
+text("STUFFED WINSTON!", 10, 30);
+textSize(10);
+text("SO SOFT! DON'T GO TO SLEEP WITHOUT ONE!", 10, 87);
+
+textSize(15);
+text("HEY! HEY! HEY!", 10, 107);
+
+textSize(20);
+text("YOU KNOW WHY ITS THE EXPLANATION!", 3,137);
